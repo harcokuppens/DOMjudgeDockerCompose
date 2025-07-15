c3e7924e8cfec99c5b2bb9a1a9b3bd9885056899
Harco Kuppens <h.kuppens@gmail.com>
2025-07-15 13:50:00 +0200

    changed installation of rust and kotlin to recent standalone packages

--- judgehost/install-extra-languages/wrapped-start.sh
+++ judgehost/install-extra-languages/wrapped-start.sh
@@ -6,7 +6,8 @@
 
 # following line copied from /scripts/start.sh which is needed to get network running right in chroot
 cp /etc/resolv.conf /chroot/domjudge/etc/resolv.conf
-# run script to install extra languages in chroot
-/opt/domjudge/judgehost/bin/dj_run_chroot /bin/install-languages
+# run script to install extra languages in chroot ; look at 'docker compose logs' to see the output
+/opt/domjudge/judgehost/bin/dj_run_chroot  /bin/install-languages  
+#/opt/domjudge/judgehost/bin/dj_run_chroot  /bin/install-languages  > /root/install-languages.log 2>&1
 # start normal judgehost start script
 exec /scripts/start.sh
